4770a88b71e87b66f9d99c76d3433c75144e0ebf
Benjamin Byl <BenjaminByl@web.de>
2016-09-10 23:47:01 +0200

    objekte auswählbar

--- WebProg2/WebContent/connecting.js
+++ WebProg2/WebContent/connecting.js
@@ -1,52 +1,69 @@
-var webSocket = new WebSocket(
-				'ws://localhost:8080/WebProg2/websocket'
-			);
-			webSocket.onerror = function(event){
-				onError(event)
-			};
-			
-			webSocket.onopen = function(event){
-				onOpen(event)
-			};
-			
-			webSocket.onmessage = function(event){
-				onMessage(event)
-			};
-			
-			function onOpen(event){
-				sendMessageToMessageBox('Connection established');
-			}
-			
-			function onError(event){
-				alert(event.data);
-			}
-			
-			function onMessage(event){
-				sendMessageToMessageBox(event.data);
-			}
-			
-			function sendMessageToMessageBox(string){
-				document.getElementById('messages').innerHTML += "<div>"+string+"</div>" ;
-			}
-			
-			function start(){
-				var text = document.getElementById("userinput").value;
-				
-				var content = {
-						type : "TEXT",
-						content : text
-					};
-				sendJSONBack("TEXT",content);
-				return false;
-			}
-			
-			function sendJSONBack(type,content){
-				if(type == "HISTORY"){
-					document.getElementById("log").innerHTML +=  "<div class='history'>"+JSON.stringify(content)+"</div>";
-				}
-				var cont = {
-						type:type,
-						content: content
-				};
-				webSocket.send(JSON.stringify(cont));
-			}
+var activeElements = [];
+var webSocket = new WebSocket('ws://localhost:8080/WebProg2/websocket');
+webSocket.onerror = function(event) {
+	onError(event)
+};
+
+webSocket.onopen = function(event) {
+	onOpen(event)
+};
+
+webSocket.onmessage = function(event) {
+	onMessage(event)
+};
+
+function onOpen(event) {
+	sendMessageToMessageBox('Connection established');
+}
+
+function onError(event) {
+	alert(event.data);
+}
+
+function onMessage(event) {
+	sendMessageToMessageBox(event.data);
+}
+
+function sendMessageToMessageBox(string) {
+	document.getElementById('messages').innerHTML += "<div>" + string
+			+ "</div>";
+}
+
+function start() {
+	var text = document.getElementById("userinput").value;
+
+	var content = {
+		type : "TEXT",
+		content : text
+	};
+	sendJSONBack("TEXT", content);
+	return false;
+}
+
+function changeAtt(e) {
+	if (e.classList.contains("active")) {
+		e.setAttribute("class", "history inActive");
+		var index = activeElements.indexOf(e);
+		activeElements.splice(index,1);
+		console.log(activeElements);
+	} else {
+		e.setAttribute("class", "active");
+		activeElements.push(e);
+	}
+}
+
+function sendJSONBack(type, content) {
+	if (type == "HISTORY") {
+		var div = document.createElement("div");
+		div.setAttribute("onclick", "changeAtt(this)");
+		div.setAttribute("class", "history inActive");
+		var text = JSON.stringify(content);
+		div.appendChild(document.createTextNode(text));
+		document.getElementById("log").appendChild(div);
+	}
+	var cont = {
+		type : type,
+		content : content
+	};
+	webSocket.send(JSON.stringify(cont));
+}
